42b7b0814dd3ccc60adc7b29516b274f18604cf6
rajsekha <rajsekhar9912@gmail.com>
2022-11-29 16:49:50 +0530

    upload changes

--- push.sh
+++ push.sh
@@ -4,10 +4,6 @@ git add .
 
 git commit -m "upload changes"
 
-git remote add origin https://github.com/rajsekha/calculator.git
-
-git remote -v
-
 echo "Enter the name of the branch"
 
 read branch 
